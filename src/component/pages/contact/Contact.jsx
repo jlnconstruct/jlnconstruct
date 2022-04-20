0656b62810e9970281d9c3cbe11c9967936137ed
Othior <splingaire.marvyn@gmail.com>
2022-04-20 16:09:07 +0200

    correction / ajout de contenu / css

--- src/component/pages/contact/Contact.jsx
+++ src/component/pages/contact/Contact.jsx
@@ -3,7 +3,30 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='container_contact'>
+        <h2 className="titre_contact">Contact</h2>
+        <form className='form_contact' action="">
+          <div className="input_nom">
+            <label htmlFor="">Nom</label>
+            <input className="input" type="text" placeholder='Nom'/>
+          </div>
+          <div className="input_phone">
+            <label htmlFor="">Téléphone</label>
+            <input className="input" type="text" placeholder='Téléphone'/>
+          </div>
+          <div className="input_email">
+            <label htmlFor="">Email</label>
+            <input className="input" type="email" placeholder='Email'/>
+          </div>
+          <div className="input_message">
+            <label htmlFor="">Message</label>
+            <textarea className="textarea" name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+          </div>
+          <button type="submit" disabled>Contacter</button>
+        </form>
+        <div className="map"></div>
+
+    </div>
   )
 }
 
