dd83e39740e04e66269e4afc355abc3569463faf
Othior <splingaire.marvyn@gmail.com>
2022-04-20 16:11:09 +0200

    teste correction

--- src/component/pages/accueil/Accueil.jsx
+++ src/component/pages/accueil/Accueil.jsx
@@ -28,7 +28,7 @@ function Accueil() {
             <h2 className="titre_description">JLN CONSTRUCT : VOTRE ENTREPRISE DE RÉNOVATION À COUTHUIN, HUY ET AUX ALENTOURS</h2>
             <p className="paragraphe_description">
              Vous préparez un projet de bâtir ou de remise à neuf ? Vous êtes à la recherche d’une entreprise de rénovation à Couthuin, 
-             Huy ou aux alentours ?<br /> Vous pouvez faire appel à ma société spécialisée dans le domaine depuis plus de 20 ans. Je mets 
+             Huy ou aux <br />  alentours ?Vous pouvez faire appel à ma société spécialisée dans le domaine depuis plus de 20 ans. Je mets 
              toute cette expérience à la disposition de vos idées et attentes. N’hésitez pas à me faire part de vos besoins !
             </p>
         </div>
